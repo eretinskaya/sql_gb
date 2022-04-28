226e6ffb9c16ae77308c72484edc8696c11b7f34
eretinskaya <104563860+eretinskaya@users.noreply.github.com>
2022-04-28 13:21:14 +0300

    Create 3_indexes.sql

---
+++ 3_indexes.sql
@@ -0,0 +1,9 @@
+-- Index for actors
+CREATE INDEX actors_first_name_last_name_idx ON actors(first_name, last_name);
+
+-- Indexes for movies
+CREATE INDEX movies_title_idx ON movies(title);
+CREATE INDEX movies_title_short_desription_idx ON movies(title, short_desription);
+
+-- Index for profiles
+CREATE INDEX profiles_first_name_last_name_idx ON profiles(first_name, last_name);
